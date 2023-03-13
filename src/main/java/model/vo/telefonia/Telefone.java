a0706f353955f0e09158336066fdec7556cc1935
lucas.araujo5 <lucas.araujo5@FLE207D08.alunos.sc.senac.br>
2023-03-13 08:30:18 -0300

    agora vai

---
+++ src/main/java/model/vo/telefonia/Telefone.java
@@ -0,0 +1,53 @@
+package model.vo.telefonia;
+
+public class Telefone {
+	
+	private Integer idCliente;
+	private String ddd;
+	private String numero;
+	private boolean ativo;
+	private boolean movel;
+	public Telefone() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Telefone(Integer idCliente, String ddd, String numero, boolean ativo, boolean movel) {
+		super();
+		this.idCliente = idCliente;
+		this.ddd = ddd;
+		this.numero = numero;
+		this.ativo = ativo;
+		this.movel = movel;
+	}
+	public Integer getIdCliente() {
+		return idCliente;
+	}
+	public void setIdCliente(Integer idCliente) {
+		this.idCliente = idCliente;
+	}
+	public String getDdd() {
+		return ddd;
+	}
+	public void setDdd(String ddd) {
+		this.ddd = ddd;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public boolean isAtivo() {
+		return ativo;
+	}
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+	public boolean isMovel() {
+		return movel;
+	}
+	public void setMovel(boolean movel) {
+		this.movel = movel;
+	}
+
+}
