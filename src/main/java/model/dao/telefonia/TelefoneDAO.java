17dd2bc523d3b3d8b4dfb3884dc2353bed53ca90
sagas <sagas@DESKTOP-0IGLB6B>
2023-03-17 23:39:53 -0300

    metodo de consulta por id adicionado na classe telefoneDAO

--- src/main/java/model/dao/telefonia/TelefoneDAO.java
+++ src/main/java/model/dao/telefonia/TelefoneDAO.java
@@ -12,13 +12,11 @@ public class TelefoneDAO {
 
 	public boolean inserir(Telefone tel1) {
 		boolean deuBom = false;
-		
+
 		Connection conn = Banco.getConnection();
-		String sql = "insert into "
-				+ "telefone (idcliente, ddd, numero, ativo, movel)"
-				+ "values (?, ?, ?, ?, ?) ";
+		String sql = " insert into " + " telefone (idcliente, ddd, numero, ativo, movel) " + " values (?, ?, ?, ?, ?) ";
 		PreparedStatement query = Banco.getPreparedStatementWithPk(conn, sql);
-		
+
 		try {
 			query.setInt(1, tel1.getIdCliente());
 			query.setString(2, tel1.getDdd());
@@ -26,27 +24,72 @@ public class TelefoneDAO {
 			query.setBoolean(4, tel1.isAtivo());
 			query.setBoolean(5, tel1.isMovel());
 			query.execute();
-			
+
 			ResultSet resultado = query.getGeneratedKeys();
-			
-			if(resultado.next()) {
+
+			if (resultado.next()) {
 				tel1.setId(resultado.getInt(1));
 			}
-			
-			deuBom = tel1.getId()>0;
-			
+
+			deuBom = tel1.getId() > 0;
+
 		} catch (SQLException e) {
-			System.out.println("Erro ao inserir telefone!"
-					+ "\nCausa: " + e.getMessage());
+			System.out.println("Erro ao inserir telefone!" + "\nCausa: " + e.getMessage());
+		} finally {
+			Banco.closePreparedStatement(query);
+			Banco.closeConnection(conn);
 		}
-		
-		
-		
-		
-		
+
 		return deuBom;
 	}
-	
-	
 
+	public void consultarTelefoneDAO(int id) {
+
+		Connection conn = Banco.getConnection();
+		String sql = " select * from telefone where id = ? ";
+		PreparedStatement query = Banco.getPreparedStatement(conn, sql);
+
+		try {
+
+			query.setInt(1, id);
+			ResultSet resultado = query.executeQuery();
+			
+			if(resultado.next()) {
+				
+				Telefone telefone = new Telefone();
+				telefone.setId(resultado.getInt("id"));
+				telefone.setIdCliente(resultado.getInt("idcliente"));
+				telefone.setDdd(resultado.getString("ddd"));
+				telefone.setNumero(resultado.getString("numero"));
+				telefone.setAtivo(resultado.getBoolean("ativo"));
+				telefone.setMovel(resultado.getBoolean("movel"));
+				
+			}
+
+		} catch (SQLException e) {
+			System.out.println("Erro ao buscar telefone com id: + " + id + "\n Causa: " + e.getMessage());
+		}finally {
+			Banco.closePreparedStatement(query);
+			Banco.closeConnection(conn);
+		}
+
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
