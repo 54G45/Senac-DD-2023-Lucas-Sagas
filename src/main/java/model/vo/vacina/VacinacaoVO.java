082d2da9ac68d3a2625ce997d83f4c8ac1ea32ba
54645 <sagas__@outlook.com>
2023-03-21 15:49:49 -0300

    Adição das entidades com construtores e Getters e Setters

---
+++ src/main/java/model/vo/vacina/VacinacaoVO.java
@@ -0,0 +1,13 @@
+package model.vo.vacina;
+
+import java.time.LocalDate;
+
+public class VacinacaoVO {
+	
+	private int id;
+	private int idVacina;
+	private int idPessoa;
+	private LocalDate data;
+	private String reacao;
+
+}
