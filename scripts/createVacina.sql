383d71d69a9cfd4c872edd544dfa117d647ed6c5
Sagaz <Sagaz@DESKTOP-393N33E>
2023-03-25 23:00:50 -0300

    criação do DB vacina + tabela vacina

--- scripts/createVacina.sql
+++ scripts/createVacina.sql
@@ -1,8 +1,23 @@
-
-
+set sql_safe_updates = 0;
 drop database if exists Vacinadb;
 create database Vacinadb;
 use Vacinadb;
+
+create table vacina(
+
+idvacina int auto_increment primary key,
+paisDeOrigem varchar(45),
+estagioDaPesquisa int,
+dataDeInicioDaPesquisa datetime,
+pesquisadorResponsavel varchar(100)
+
+);
+
+insert into vacina (paisDeOrigem, estagioDaPesquisa, dataDeInicioDaPesquisa, pesquisadorResponsavel)
+values ();
+
+select * from vacina;
+
 
 
 
