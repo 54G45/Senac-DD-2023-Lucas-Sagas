ee63d9a54c3ccd69704dbfb117a239e35c78848a
54645 <sagas__@outlook.com>
2023-03-26 22:03:41 -0300

    adição da tabela DAO no banco "vacinadb"

--- scripts/createVacina.sql
+++ scripts/createVacina.sql
@@ -13,6 +13,19 @@ pesquisadorResponsavel varchar(100)
 
 );
 
+create table pessoa(
+idpessoa int auto_increment,
+nome varchar(100) not null,
+dataNascimento datetime,
+sexo char,
+cpf varchar(11),
+tipo varchar(45)
+
+);
+
+insert into pessoa()
+values ();
+
 insert into vacina (paisDeOrigem, estagioDaPesquisa, dataDeInicioDaPesquisa, pesquisadorResponsavel)
 values ();
 
